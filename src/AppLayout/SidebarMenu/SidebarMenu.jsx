3a32f8941e2005ea260ef840e6e6a627498c644f
wiem-2001 <wiembm2001@gmail.com>
2025-07-14 23:53:51 +0100

    ui fixes

--- src/AppLayout/SidebarMenu/SidebarMenu.jsx
+++ src/AppLayout/SidebarMenu/SidebarMenu.jsx
@@ -99,7 +99,7 @@ useEffect(() => {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      style={{ verticalAlign: 'middle' }}
+      style={{ verticalAlign: 'middle' ,marginRight:'15px'}}
     >
       <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" />
       <path d="M13 5v2" />
